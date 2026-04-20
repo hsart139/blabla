0bf712951b103718bc393877a4e96dc2e0c4d314
hsart139 <bartekchylinski07@gmail.com>
2026-04-20 23:48:54 +0200

    Add homerun, echo, ghost, and heavy_trigger upgrades to greatsword and katana.

---
+++ weapon_presets.gd
@@ -0,0 +1,17 @@
+# Weapon Presets
+
+# Greatsword upgrades
+upgrades.greatsword = [
+    'homerun',
+    'echo',
+    'ghost',
+    'heavy_trigger',
+]
+
+# Katana upgrades
+upgrades.katana = [
+    'homerun',
+    'echo',
+    'ghost',
+    'heavy_trigger',
+]
